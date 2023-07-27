1cda216a3e770ca40ec9c910a3b3a6923889f46d
CodingClassNC <140469733+CodingClassNC@users.noreply.github.com>
2023-07-27 02:21:01 +0000

    s

--- Image+Emotion/index.js
+++ Image+Emotion/index.js
@@ -2,11 +2,51 @@ prediction2 = "";
 prediction1 = "";
 
 Webcam.set({
-    width:450,
-    height:450,
+    width:500,
+    height:500,
     image_format:"png",
     png_quality: 90
 });
 
 camera = document.getElementById("camera");
 Webcam.attach('#camera');
+
+function snapshot(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = '<img id="captureimg" src="'+data_uri+'"/>';
+    });
+}
+
+console.log("ml5version", ml5.version);
+classifier = ml5.imageClassifier('https://storage.googleapis.com/tm-model/5mxzZHkpx/model.json', modelLoaded);
+
+function modelLoaded(){
+    console.log('Modal Loaded');
+}
+
+function speak(){
+    var synth = window.speechSynthesis;
+    speekdata1 = "The First Prediction is " + prediction1;
+    speekdata2 = "The Second Prediction is " + prediction2;
+    var utterThis = new SpeechSynthesisUtterance(speekdata1 + speekdata2);
+    synth.speak(utterThis);
+}
+
+function check(){
+    img = document.getElementById("captureimg");
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results){
+    if (error){
+        console.error(error)
+    }
+    else{
+        console.log(results);
+    }
+    document.getElementById("resultemotionname").innerHTML = results[0].label;
+    document.getElementById("resultemotionname2").innerHTML = results[1].label;
+    prediction1 = results[0].label;
+    prediction2 = results[1].label;
+    speak();
+}
