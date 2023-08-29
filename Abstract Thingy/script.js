f7f6a3767319bebde09b39a281d7732ba7a3c740
CodingClassNC <140469733+CodingClassNC@users.noreply.github.com>
2023-08-29 01:58:09 +0000

    sad

---
+++ Abstract Thingy/script.js
@@ -0,0 +1,63 @@
+const artworks = [
+    {
+        title: "Abstract Kid",
+        artist: "Random Kid",
+        description: "bye bye kid",
+        interactive: false
+    },
+    {
+        title: "Weird Kid Land",
+        artist: "A Ghost Watching You",
+        description: "Basically yourself in 2 minutes",
+        interactive: true
+    }
+];
+
+let currentartwork = null;
+
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+    noLoop();
+    const artworkContainer = select("#artworkcontainer"); 
+    for (let i = 0; i < artworks.length; i++) {
+        const artworkdiv = createDiv();
+        artworkdiv.class("artwork");
+        artworkdiv.elt.addEventListener("click", () => selectartwork(i));
+        const artworktitle = createElement("h2", artworks[i].title);
+        const artistname = createElement("p", "by someone named: " + artworks[i].artist);
+        artworkdiv.child(artworktitle);
+        artworkdiv.child(artistname);
+        artworkContainer.child(artworkdiv);
+    }
+}
+
+function draw() {
+    background(63, 104, 224);
+    if (currentartwork !== null) {
+        currentartwork.display();
+    }
+}
+
+function selectartwork(index) {
+    if (artworks[index].interactive) {
+        currentartwork = new interactiveArtwork();
+    } else {
+        currentartwork = new staticArtwork();
+    }
+    redraw();
+}
+
+class staticArtwork {
+    display() {
+        fill(random(255), random(255), random(255));
+        ellipse(width / 2, height / 2, 200);
+    }
+}
+
+class interactiveArtwork {
+    display() {
+        fill(100, 0, 0);
+        rectMode(CENTER);
+        rect(width / 2, height / 2, 200, 200);
+    }
+}
