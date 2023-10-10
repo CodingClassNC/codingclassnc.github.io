ce67c77f5b49d23f2bcb04143019af7b724e5a52
CodingClassNC <140469733+CodingClassNC@users.noreply.github.com>
2023-10-10 02:26:46 +0000

    asdasd

--- Blooper/script.js
+++ Blooper/script.js
@@ -9,68 +9,96 @@ const firebaseConfig = {
     measurementId: "G-LK0BY0E7FN"
   };
 
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-const database = firebase.database();
-const messagesRef = database.ref('messages');
-
-const usernameInput = document.getElementById('username-input');
-const startButton = document.getElementById('start-button');
-const chatContainer = document.getElementById('chat-container');
-const displayUsername = document.getElementById('display-username');
-
-startButton.addEventListener('click', () => {
-    const username = usernameInput.value.trim();
-
-    if (username !== '') {
-        chatContainer.style.display = 'block';
-        document.getElementById('get-started').style.display = 'none';
-        displayUsername.textContent = username;
-    }
-});
-
-const messageInput = document.getElementById('message-input');
-const sendButton = document.getElementById('send-button');
-const messagesDiv = document.getElementById('messages');
-
-sendButton.addEventListener('click', () => {
-    const messageText = messageInput.value.trim();
-
-    if (messageText !== '') {
-        const message = {
-            text: messageText,
-            username: displayUsername.textContent,
-            timestamp: firebase.database.ServerValue.TIMESTAMP
-        };
-
-        messagesRef.push(message);
-        messageInput.value = '';
-    }
-});
-
-messagesRef.on('child_added', (snapshot) => {
+  firebase.initializeApp(firebaseConfig);
+  const database = firebase.database();
+  const messagesRef = database.ref('messages');
+  
+  const usernameInput = document.getElementById('username-input');
+  const profileImgInput = document.getElementById('profile-img-input'); 
+  const startButton = document.getElementById('start-button');
+  const chatContainer = document.getElementById('chat-container');
+  const displayUsername = document.getElementById('display-username');
+  const profileImg = document.getElementById('profile-img'); 
+  
+  startButton.addEventListener('click', () => {
+      const username = usernameInput.value.trim();
+      const profileImgUrl = profileImgInput.value.trim(); 
+      if (username !== '') {
+          chatContainer.style.display = 'block';
+          document.getElementById('get-started').style.display = 'none';
+          displayUsername.textContent = username;
+  
+          // Set the profile image
+          if (profileImgUrl !== '') {
+              profileImg.src = profileImgUrl;
+              profileImg.alt = `Profile Image of ${username}`;
+          }
+      }
+  });
+  
+  const messageInput = document.getElementById('message-input');
+  const sendButton = document.getElementById('send-button');
+  const messagesDiv = document.getElementById('messages');
+  
+  sendButton.addEventListener('click', () => {
+      const messageText = messageInput.value.trim();
+  
+      if (messageText !== '') {
+          const message = {
+              text: messageText,
+              username: displayUsername.textContent,
+              profileImgUrl: profileImg.src, 
+              timestamp: firebase.database.ServerValue.TIMESTAMP
+          };
+  
+          messagesRef.push(message);
+          messageInput.value = '';
+      }
+  });
+  
+  messagesRef.on('child_added', (snapshot) => {
     const message = snapshot.val();
     const messageElement = document.createElement('div');
-    messageElement.textContent = message.text;
+    messageElement.classList.add('message-container'); 
 
-    if (message.username === displayUsername.textContent) {
-        // Messages from the user
-        messageElement.classList.add('message', 'sender');
-    } else {
-        // Messages from others
-        messageElement.classList.add('message', 'receiver');
-    }
+    const profileImgElement = document.createElement('img');
+    profileImgElement.src = message.profileImgUrl;
+    profileImgElement.alt = `Profile Image of ${message.username}`;
+    profileImgElement.classList.add('profile-img');
+
+    const userInfoElement = document.createElement('div'); 
+    userInfoElement.classList.add('user-info');
 
     const usernameElement = document.createElement('div');
     usernameElement.textContent = message.username;
     usernameElement.classList.add('username');
 
+    const textElement = document.createElement('div');
+    textElement.textContent = message.text;
+    textElement.classList.add('message-text');
+
     const timestampElement = document.createElement('div');
     timestampElement.textContent = formatTimestamp(message.timestamp);
     timestampElement.classList.add('timestamp');
 
-    messageElement.appendChild(usernameElement);
-    messageElement.appendChild(timestampElement);
+    if (message.username === displayUsername.textContent) {
+        messageElement.classList.add('message', 'sender');
+        messageElement.appendChild(profileImgElement);
+        usernameElement.classList.add('sender-username');
+        userInfoElement.appendChild(usernameElement);
+        userInfoElement.appendChild(textElement);
+        messageElement.appendChild(userInfoElement);
+        messageElement.appendChild(timestampElement);
+    } else {
+        messageElement.classList.add('message', 'receiver');
+        messageElement.appendChild(profileImgElement);
+        usernameElement.classList.add('receiver-username');
+        userInfoElement.appendChild(usernameElement);
+        userInfoElement.appendChild(textElement);
+        messageElement.appendChild(userInfoElement);
+        messageElement.appendChild(timestampElement);
+    }
+
     messagesDiv.appendChild(messageElement);
     messagesDiv.scrollTop = messagesDiv.scrollHeight;
 });
@@ -79,3 +107,4 @@ function formatTimestamp(timestamp) {
     const date = new Date(timestamp);
     return date.toLocaleTimeString();
 }
+
